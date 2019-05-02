a15a77c7975d88360d92c0eaee4132b8614fd526
Michael Bauer <michael.bauer@jacks.sdstate.edu>
2019-05-02 18:51:12 -0500

    yee

--- src/data-store/bagels.ts
+++ src/data-store/bagels.ts
@@ -1,12 +1,15 @@
 import { Bagel } from "../models/bagel";
-import { Observable } from "rxjs";
-import { of } from "rxjs/observable/of";
 
-export class Bagels{
-    bagels: Bagel[] = [
-        {name: "baby", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined}];
-
-    getBagels(): Observable<Bagel[]>{
-        return of(this.bagels);
-    }
-}
+export const BAGELS : Bagel[] = [
+        {name: "Plain", price: 5.00, imageLocation: "", description: "Classic Bagel.", smear: undefined, addOns: undefined},
+        {name: "Cinnamon Raisen", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Honey Whole Wheat", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Asiago", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Chocolate Chip", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "French Toast", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Cinnamon Sugar", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Garlic", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Poppy Seed", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Sesame Seed", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Potato", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
+        {name: "Pretzel", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined}];
