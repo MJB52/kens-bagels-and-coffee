e22a0372cc518e501a1af5976f07b1a317742e80
Michael Bauer <michael.bauer@jacks.sdstate.edu>
2019-05-02 18:53:23 -0500

    removed some shit

--- src/data-store/bagels.ts
+++ src/data-store/bagels.ts
@@ -1,15 +1,15 @@
 import { Bagel } from "../models/bagel";
 
 export const BAGELS : Bagel[] = [
-        {name: "Plain", price: 5.00, imageLocation: "", description: "Classic Bagel.", smear: undefined, addOns: undefined},
-        {name: "Cinnamon Raisen", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Honey Whole Wheat", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Asiago", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Chocolate Chip", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "French Toast", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Cinnamon Sugar", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Garlic", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Poppy Seed", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Sesame Seed", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Potato", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined},
-        {name: "Pretzel", price: 5.00, imageLocation: "", description: "", smear: undefined, addOns: undefined}];
+        {name: "Plain", price: 5.00, imageLocation: "", description: "Classic Bagel."},
+        {name: "Cinnamon Raisen", price: 5.00, imageLocation: "", description: ""},
+        {name: "Honey Whole Wheat", price: 5.00, imageLocation: "", description: ""},
+        {name: "Asiago", price: 5.00, imageLocation: "", description: ""},
+        {name: "Chocolate Chip", price: 5.00, imageLocation: "", description: ""},
+        {name: "French Toast", price: 5.00, imageLocation: "", description: ""},
+        {name: "Cinnamon Sugar", price: 5.00, imageLocation: "", description: ""},
+        {name: "Garlic", price: 5.00, imageLocation: "", description: ""},
+        {name: "Poppy Seed", price: 5.00, imageLocation: "", description: ""},
+        {name: "Sesame Seed", price: 5.00, imageLocation: "", description: ""},
+        {name: "Potato", price: 5.00, imageLocation: "", description: ""},
+        {name: "Pretzel", price: 5.00, imageLocation: "", description: ""}];
