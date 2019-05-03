9d5b037229c4cecc5e1e997359cf82cc2c1b439d
Michael Bauer <michael.bauer@jacks.sdstate.edu>
2019-05-02 20:40:09 -0500

    yet

--- src/pages/cart-page/cart-page.ts
+++ src/pages/cart-page/cart-page.ts
@@ -4,7 +4,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-cart',
-  templateUrl: 'cart.html',
+  templateUrl: 'cart-page.html',
 })
 export class CartPage {
 
