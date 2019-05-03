b3a1094b0554415162dc4b1081ecd00824c43660
Michael Bauer <michael.bauer@jacks.sdstate.edu>
2019-05-02 21:36:12 -0500

    view item details works

--- src/pages/cart-page/cart-page.ts
+++ src/pages/cart-page/cart-page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Cart } from '../../data-store/cart';
+import { Bagel } from '../../models/bagel';
 
 @IonicPage()
 @Component({
@@ -7,12 +9,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cart-page.html',
 })
 export class CartPage {
-
+  bagels: Bagel[] = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
     
   }
 
+  ionViewDidLoad() {
+    Cart.getBagels().subscribe(items => {
+      this.bagels = items;
+    });
+  }
+  
+  getItems(){
+    Cart.getBagels().subscribe(items => {
+      console.log(items);
+      this.bagels = items;
+    });
+  }
 }
